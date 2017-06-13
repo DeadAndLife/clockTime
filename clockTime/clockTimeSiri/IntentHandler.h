23ccabf7b493bc8e580772da5c9125c149f9e9db
张小东 <2431735199@qq.com>
2017-06-13 15:14:02 +0800

    增加各类配置文件以便更新数据库

---
+++ clockTime/clockTimeSiri/IntentHandler.h
@@ -0,0 +1,13 @@
+//
+//  IntentHandler.h
+//  clockTimeSiri
+//
+//  Created by iOSDeveloper on 2017/5/12.
+//  Copyright © 2017年 iOSDeveloper. All rights reserved.
+//
+
+#import <Intents/Intents.h>
+
+@interface IntentHandler : INExtension
+
+@end
---
+++ clockTime/clockTimeSiriUI/IntentViewController.h
@@ -0,0 +1,13 @@
+//
+//  IntentViewController.h
+//  clockTimeSiriUI
+//
+//  Created by iOSDeveloper on 2017/5/12.
+//  Copyright © 2017年 iOSDeveloper. All rights reserved.
+//
+
+#import <IntentsUI/IntentsUI.h>
+
+@interface IntentViewController : UIViewController <INUIHostedViewControlling>
+
+@end
